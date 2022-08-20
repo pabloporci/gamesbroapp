d77dfd237d6269f4445d6d8c3a768ce0864d6178
Pablo porciuncula <pablogonzaloporciuncula16@gmail.com>
2022-08-20 19:06:19 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './estilos.css';
 
 function App() {
+  let imagen = "godwarragnarok.jpg";
+  let titulo = "god war ragnarock";
+  let consola = "playstation";
+  let descripcion = "juego de accion";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='row'>
+        <div className='col-md-4 offset-md-2'>
+          <img src={imagen} alt={titulo} className="img-fluid" />
+        </div>
+        <div className='col-md-4'>
+          <h1>{titulo}</h1>
+          <p>{descripcion}</p>
+        </div>
+      </div>
     </div>
   );
 }
