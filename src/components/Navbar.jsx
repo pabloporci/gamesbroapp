00a803671dd3cf043e111c7621a3b65527c5578d
Pablo porciuncula <pablogonzaloporciuncula16@gmail.com>
2022-08-25 20:29:13 -0300

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from "./images/favicon.png";
+
+const Navbar = () => {
+  return (
+    <div className="container">
+      <h1 className="letra_titulo">Games Bro</h1>
+      <h3 className="letra_titulo">El sitio gamer hecho para vos</h3>
+      <ul class="nav d-flex align-items-center">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#"><img src={logo} width="48" alt="Games Bro" /></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link link_header" href="#">Inicio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link link_header" href="#">Tienda</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link link_header" href="#">Consolas</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link link_header" href="#">Preventa</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link link_header" href="#">Contacto</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar;
